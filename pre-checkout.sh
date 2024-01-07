5fdb649f6e395113c9c225b27ec9e2efa2c56406
Xi3xi <97969553+Xi3xi@users.noreply.github.com>
2024-01-07 22:57:43 +1100

    modify pre-checkout process

--- pre-checkout.sh
+++ pre-checkout.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sleep 10
+sleep 5
+RESULT_FILE="testing.json"
 
 # Get the comma-separated list of IDs
 ID_LIST=$(buildkite-agent meta-data get us)
@@ -13,17 +14,18 @@ IFS=',' read -r -a ids <<< "$ID_LIST"
 USER_STORIES=$(cat user-stories.json)
 
 # Decode JSON and filter based on IDs
-echo "[]" > testing.json # Initialize testing.json with an empty array
+echo "[]" > "$RESULT_FILE" # Initialize testing.json with an empty array
 
 for id in "${ids[@]}"; do
     # Extract the user story with the matching ID and append to testing.json
     USER_STORY=$(echo "$USER_STORIES" | jq --arg id "$id" '.user_stories[] | select(.id == ($id | tonumber))')
     if [ ! -z "$USER_STORY" ]; then
         # Append to testing.json
-        jq -s '.[0] + [.[1]]' testing.json <(echo "$USER_STORY") > temp.json && mv temp.json testing.json
+        jq -s '.[0] + [.[1]]' "$RESULT_FILE" <(echo "$USER_STORY") > temp.json && mv temp.json testing.json
     fi
 done
 
-echo "Filtered user stories stored in testing.json"
+echo "Filtered user stories stored in $RESULT_FILE: "
+cat "$RESULT_FILE"
 
 sleep 5
